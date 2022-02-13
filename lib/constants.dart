f117fa89c8335e1b785755c8ca3a5d59dcb99af4
Dev Akabari <devakabari@Devs-MacBook-Pro.local>
2022-02-13 16:37:58 +0530

    code1

---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+Color primaryTextColor = Color(0xFF414C6B);
+Color secondaryTextColor = Color(0xFFE4979E);
+Color titleTextColor = Colors.white;
+Color contentTextColor = Color(0xff868686);
+Color navigationColor = Color(0xFF6751B5);
+Color gradientstartlolor = Color(0xff0030AC);
+Color gradientEndColor = Color(0xFF9354B9);
